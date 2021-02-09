9d44fe6e5227e162e357fe458e8658c0ba988d4f
Kim Rasmussen <68238123+kimrass14@users.noreply.github.com>
2021-02-09 15:01:21 -0600

    dedication

--- src/Components/ForYou/ForYou.js
+++ src/Components/ForYou/ForYou.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import Zoom from 'react-reveal/Zoom'
 import './ForYou.scss'
 
 const ForYou = () => {
 
     return(
         <div className="forYou section">
-            <h2>For You...</h2>
+            <h2>To the....</h2>
+            <p><span className="role">WIFE</span> so devoted that finally got her husband to eat and love cheesecake</p>
+            <p><span className="role">MOTHER</span> that leads by example with the fullest of hearts</p>
+            <p><span className="role">SISTER</span> that stands unwavering at her sisters' side</p>
+            <p><span className="role">DAUGHTER</span> that cooks like her mother and creates with her hands like her father</p>
+            <p><span className="role">AUNT</span> with the greatest admiration of her nieces and nephews</p>
+            <p><span className="role">GRANDMA</span> that teaches (litterally) and so playful</p>
+            <p><span className="role">FRIEND</span> that exemplifies the truest definition of the word</p>
+            <p><span className="role">TEACHER</span> so impressionable she continues to inspire students she taught decades ago</p> 
+            <p><span className="role">EVERYTHING</span> else in between that you are:</p>
+            <Zoom right cascade><h2 className="love-you">WE LOVE YOU!</h2></Zoom>
         </div>
     )
 }
