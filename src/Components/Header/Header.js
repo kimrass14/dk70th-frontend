a87f33293fa8ffe33298c891a41473a7468dc9a5
Kim Rasmussen <68238123+kimrass14@users.noreply.github.com>
2021-02-10 12:40:33 -0600

    added audio player with hbd song

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,6 +3,10 @@ import {Link} from 'react-router-dom'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faCrown} from '@fortawesome/free-solid-svg-icons'
 import './Header.scss'
+import ReactAudioPlayer from 'react-audio-player'
+import mp3 from './dynamite.mp3'
+import hbd from './HappyBirthday.mp3'
+
 
 const Header = () => {
 
@@ -10,6 +14,13 @@ const Header = () => {
         <header><Link to='/'>
             <FontAwesomeIcon icon={faCrown}/> Diane Kosuda
              </Link>
+            <ReactAudioPlayer
+                src={hbd}
+                autoPlay
+                loop
+                controls
+                className="audio"
+            />
         </header>
     )
 }
